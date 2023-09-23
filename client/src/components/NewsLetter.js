1cfe83659ea9f94943f3f1219e9da21a102552f4
shehabull <shehabull@gmail.com>
2023-09-23 23:07:14 +0600

    added Newsletter component

---
+++ client/src/components/NewsLetter.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const NewsLetter = () => {
+  return (
+    <div className="max-w-[1520px] mx-auto px-4 text-white bg-[#24262b]">
+      <div className="max-auto grid lg:grid-cols-3">
+        <div className="lg:col-span-2 my-4">
+          <h1>Need advice on how to improve your flow?</h1>
+          <p>Sign Up to join our newsletter and stay up to date</p>
+        </div>
+        <div className="my-4">
+          <div className="flex flex-col sm:flex-row items-center justify-between w-full">
+            <input
+              type="email"
+              className="p-3 flex w-full rounded-md text-black"
+              placeholder="Enter your Email"
+            />
+            <button className="bg-[#00dfd0] text-white rounded-md font-medium w-[200px] ml-4 my-6 px-6 py-3 border-none">
+              Notify Me
+            </button>
+          </div>
+          <p>
+            We are concerned about the security of your data, Read {""}
+            <span className="text-[#00dfd0]">Privacy Policy</span>
+          </p>
+        </div>
+
+        <hr className="my-8 bg-gray-700 border-1" />
+      </div>
+    </div>
+  );
+};
+
+export default NewsLetter;
